7b65222ee5b10f4d6e6d641d4aa5b2d75250e1da
profmarciobarua <143102533+profmarciobarua@users.noreply.github.com>
2024-03-12 20:09:11 -0400

    teste

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,5 @@
+use App\Http\Controllers\TaskController;
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
